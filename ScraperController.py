45f9960f019e358f72eacfcee3899677761deef2
Yukthiw <yukthiw61@gmail.com>
2022-01-08 13:02:32 -0500

    Created Scraper superclass and made all scrapers a subclass. Also began with scraper controller

---
+++ ScraperController.py
@@ -0,0 +1,7 @@
+
+class ScraperController():
+    def __init__(self):
+        pass
+
+    print("Welcome to Python Scraper Tool v1 \n Please choose what data you'd like to scrape:")
+    print("1. Fpl Api \n 2. Player Data \n 3. Team Data \n 4. Full Data ")
---
+++ scraper.py
@@ -0,0 +1,6 @@
+
+class Scraper:
+
+    def __init__(self, url, target):
+        self.url = url
+        self.target = target
